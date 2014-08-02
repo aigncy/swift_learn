f19fc4ba4734dd1bbde6b54996411b6dd3785f96
aigncy <aigncy@gmail.com>
2014-08-02 15:50:17 -0400

    first

---
+++ test.playground/section-1.swift
@@ -0,0 +1,16 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+println("hello world")
+
+for character in "Dog!🐶" {
+    println(character)
+}
+
+for character in "swift" {
+    println(character)
+}
+
